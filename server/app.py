993d064abd3ec320071522c729397e0576b66d2f
Connor Page <george@connorpage.com>
2024-08-23 12:13:58 -0500

    Completes lab

--- server/app.py
+++ server/app.py
@@ -14,13 +14,37 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
+# GET /messages: return all messages ordered by created_at
+@app.route('/messages', methods=['GET'])
+def get_messages():
+    messages = Message.query.order_by(Message.created_at.asc()).all()
+    return jsonify([message.to_dict() for message in messages])
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+# POST /messages: create a new message
+@app.route('/messages', methods=['POST'])
+def create_message():
+    data = request.get_json()
+    new_message = Message(body=data.get('body'), username=data.get('username'))
+    db.session.add(new_message)
+    db.session.commit()
+    return jsonify(new_message.to_dict()), 201
 
-if __name__ == '__main__':
-    app.run(port=5555)
+# PATCH /messages/<int:id>: update a message's body
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    message = Message.query.get_or_404(id)
+    data = request.get_json()
+    message.body = data.get('body', message.body)
+    db.session.commit()
+    return jsonify(message.to_dict())
+
+# DELETE /messages/<int:id>: delete a message
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = Message.query.get_or_404(id)
+    db.session.delete(message)
+    db.session.commit()
+    return make_response('', 204)
+
+if __name__ == '__main__': 
+    app.run(port=5000)
